dc39734de6b02c748474b9d44287a63f36b7d0df
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-02-27 07:46:03 +0100

    Specifying the interface

--- mdns.js
+++ mdns.js
@@ -1,7 +1,9 @@
-import _mdns    from 'multicast-dns'
+import _mdns from 'multicast-dns'
 
 export default function(args) {
-  let mdns = _mdns()
+  let mdns = _mdns({
+    interface: args.address
+  })
   mdns.on('query', q => {
     if (args['debug-mdns']) console.log('MDNS QUERY', q)
     let swarmQuery = q.questions.reduce((found, question) => {
